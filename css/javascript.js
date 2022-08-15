a8b744ec3246bc15c2e979d66ed7e22ebdcbf21a
marouf <maroufibrahimi7264@gmail.com>
2022-08-15 14:08:53 -0700

    some change to code

--- css/javascript.js
+++ css/javascript.js
@@ -1,4 +1,23 @@
+// const slider = document.querySelectorAll('.slides');
 
+// const nextImgDelay = 3000;
+// let currentImg = 0;
+
+// slider[currentImg].style.opacity = 1;
+// setInterval(() => {
+//     autoNextImage();
+// }, nextImgDelay);
+
+// function autoNextImage() {
+//     slider[currentImg].style.zIndex = -2;
+//     const temporaryCounter = currentImg;
+//     setTimeout(() => {
+//         slider[temporaryCounter].style.opacity = 0;
+//     }, 1000);
+//     currentImg = (currentImg + 1) % slider.length;
+//     slider[currentImg].style.opacity = 1;
+//     slider[currentImg].style.zIndex = -1;
+// }
 
 // Script for slider
 var index = 1;
@@ -16,15 +35,15 @@ function slideShow(n) {
     var a;
     var slides = document.getElementsByClassName('slides');
     var dots = document.getElementsByClassName('dot');
-    if(n > slides.length) {
+    if (n > slides.length) {
         index = 1;
-    }else if (n < 1) {
+    } else if (n < 1) {
         index = slides.length;
     }
-    for(a=0; a < slides.length; a++) {
+    for (a = 0; a < slides.length; a++) {
         slides[a].style.display = "none";
     }
-    for(a = 0; a < dots.length; a++) {
+    for (a = 0; a < dots.length; a++) {
         dots[a].className = dots[a].className.replace(" active", "");
     }
     slides[index - 1].style.display = "block";
@@ -32,29 +51,30 @@ function slideShow(n) {
 }
 // end of slider
 
+const side_menu = document.getElementById('side-menu');
 // Open slide menu
 function openSlideMenu() {
-    document.getElementById('side-menu').style.width = '100%';
+    side_menu.style.width = '100%';
 }
 // Close slide menu
 function closeSlideMenu() {
-    document.getElementById('side-menu').style.width = '0';
+    side_menu.style.width = '0';
 }
 
-// Up arrow
+// Up arrow (IIFE Function)
 (function scrollToTop() {
     var goUp = document.getElementById('up-arrow');
 
     function setGoUpBtnVisibality() {
-        if(window.scrollY <= 150){
+        if (window.scrollY <= 150) {
             goUp.fadeOut();
-        }else {
+        } else {
             goUp.fadeIn();
         }
     }
 
     window.addEventListener('scroll', setGoUpBtnVisibality);
-    goUp.addEventListener('click', function(){
+    goUp.addEventListener('click', function () {
         this.scrollTo(0, 0);
     });
 }());
@@ -63,7 +83,7 @@ function closeSlideMenu() {
 function myFadeIn(ele) {
     var elementOpa = Number(getComputedStyle(ele).opacity).toPrecision(2);
     ele.style.opacity = elementOpa;
-    if(elementOpa >= 1) {
+    if (elementOpa >= 1) {
         return;
     }
 
@@ -76,7 +96,7 @@ function myFadeIn(ele) {
 function myFadeOut(ele) {
     var elementOpa = Number(getComputedStyle(ele).opacity).toPrecision(2);
 
-    if(elementOpa <= 0){
+    if (elementOpa <= 0) {
         return;
     }
 
@@ -86,10 +106,10 @@ function myFadeOut(ele) {
     }, 10);
 }
 
-Element.prototype.fadeIn = function() {
+Element.prototype.fadeIn = function () {
     myFadeIn(this);
 }
-Element.prototype.fadeOut = function() {
+Element.prototype.fadeOut = function () {
     myFadeOut(this);
 }
 // End of Up arrow
